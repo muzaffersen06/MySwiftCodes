7dcf353168b9d7b280323ab6b48d06f31d587e1c
mbaransen <94108242+mbaransen@users.noreply.github.com>
2023-07-04 11:43:29 +0300

    Add files via upload

---
+++ Login Uygulama/Login Uygulama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login Uygulama
+//
+//  Created by Muzaffer Baran on 1.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Login Uygulama/Login UygulamaUITests/Login_UygulamaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Login_UygulamaUITestsLaunchTests.swift
+//  Login UygulamaUITests
+//
+//  Created by Muzaffer Baran on 1.06.2023.
+//
+
+import XCTest
+
+final class Login_UygulamaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
